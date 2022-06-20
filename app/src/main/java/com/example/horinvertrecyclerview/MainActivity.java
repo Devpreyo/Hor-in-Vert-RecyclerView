f388f0bfd4e1928c4df7ba5f36bdb310dc41f5ce
Devpreyo <roy.devpreyo@ubuy.com>
2022-06-20 17:53:09 +0530

    firstcommit

---
+++ app/src/main/java/com/example/horinvertrecyclerview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.horinvertrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
